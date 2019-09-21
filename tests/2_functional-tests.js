9dba43ada1ec47478936956a9f785eaf35dbdb7b
Glitch (fcc-stock-price-checker-tf) <none>
2019-09-21 14:27:19 +0000

    Complete logic and functional tests.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -13,39 +13,84 @@ var server = require('../server');
 
 chai.use(chaiHttp);
 
-/*suite('Functional Tests', function() {
+suite('Functional Tests', function() {
     
     suite('GET /api/stock-prices => stockData object', function() {
       
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: 'goog'})
+        .query({stock: 'snv'})
         .end(function(err, res){
           if (err) console.error(err)
-          //complete this one too
-          
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData.stock, 'SNV')
+          assert.property(res.body.stockData, 'price')
+          assert.property(res.body.stockData, 'likes')
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'snv', like: true})
+        .end(function(err, res){
+          if (err) console.error(err)
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData.stock, 'SNV')
+          assert.equal(res.body.stockData.likes, 1)
+          assert.property(res.body.stockData, 'price')
+          done();
+        });
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'snv', like: true})
+        .end(function(err, res){
+          if (err) console.error(err)
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData.stock, 'SNV')
+          assert.equal(res.body.stockData.likes, 1)
+          assert.property(res.body.stockData, 'price')
+          done();
+        });
       });
       
       test('2 stocks', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: [ 'snv', 'xlnx' ] })
+        .end(function(err, res){
+          if (err) console.error(err)
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData[0].stock, 'SNV')
+          assert.equal(res.body.stockData[0].rel_likes, 1)
+          assert.property(res.body.stockData[0], 'price')
+          assert.equal(res.body.stockData[1].stock, 'XLNX')
+          assert.equal(res.body.stockData[1].rel_likes, -1)
+          assert.property(res.body.stockData[1], 'price')
+          done();
+        });
       });
       
       test('2 stocks with like', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: [ 'snv', 'stx' ], like: true })
+        .end(function(err, res){
+          if (err) console.error(err)
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData[0].stock, 'SNV')
+          assert.equal(res.body.stockData[0].rel_likes, 0)
+          assert.property(res.body.stockData[0], 'price')
+          assert.equal(res.body.stockData[1].stock, 'STX')
+          assert.equal(res.body.stockData[1].rel_likes, 0)
+          assert.property(res.body.stockData[1], 'price')
+          done();
+        });
       });
-      
     });
-
 });
-*/
